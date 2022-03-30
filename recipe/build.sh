565aba75757594928b7b363fe28d1a58e20609d2
Nicholas Bollweg <nick.bollweg@gmail.com>
2022-03-29 21:13:36 -0500

    use gradle, add version check

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,19 +3,10 @@ set -eux
 
 export MAVEN_OPTS="-Xmx1G"
 
-cd $SRC_DIR
+gradle -q clean build pdfJar -x test
 
-mvn --batch-mode versions:set "-DnewVersion=v${PKG_VERSION}"
-
-# skip simple tests because of font matching on CI machines
-mvn --batch-mode \
-    -Dmaven.javadoc.skip=true \
-    -Dmaven.source.skip=true \
-    -Dtest='!*nonreg.simple.A000*' \
-    package
-
-cp "${SRC_DIR}/target/plantuml-v${PKG_VERSION}.jar" "${PREFIX}/lib/"
+cp build/libs/plantuml-pdf-*.jar "${PREFIX}/lib/plantuml.jar"
 
 echo '#!/bin/bash' > $PREFIX/bin/plantuml
-echo 'java -Xmx500M -jar "'$PREFIX'/lib/plantuml-v'$PKG_VERSION'.jar" "$@"' >> $PREFIX/bin/plantuml
+echo 'java -Xmx500M -jar "'$PREFIX'/lib/plantuml.jar" "$@"' >> $PREFIX/bin/plantuml
 chmod +x "${PREFIX}/bin/plantuml"
