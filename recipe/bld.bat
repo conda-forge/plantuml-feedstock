b4574c793d987c3e251c691072849dfe8febc05a
AjayP <Ajay.Madhavan@in.bosch.com>
2023-01-27 14:45:45 +0530

    Fix win build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,20 +1,8 @@
 @ECHO ON
 
-set MAVEN_OPTS="-Xmx1G"
+CALL gradlew.bat
+CALL gradlew clean build pdfJar -x test
 
-gradle -q clean build pdfJar -x test
+CALL echo F | xcopy .\build\libs\plantuml-pdf-*.jar %LIBRARY_LIB%\plantuml.jar
 
-if not exist %LIBRARY_LIB% mkdir %LIBRARY_LIB%
-
-copy build\libs\plantuml-pdf-*.jar "%LIBRARY_LIB%\plantuml-pdf.jar" || goto :error
-
-if not exist %LIBRARY_BIN% mkdir %LIBRARY_BIN%
-
-echo java -Xmx500M -jar %LIBRARY_LIB%\plantuml-pdf.jar %%* > %LIBRARY_BIN%\plantuml.cmd
-echo IF %%ERRORLEVEL%% NEQ 0 EXIT /B %%ERRORLEVEL%% >> %LIBRARY_BIN%\plantuml.cmd
-
-type %LIBRARY_BIN%\plantuml.cmd
-
-:error
-echo Failed with error #%errorlevel%.
-exit /b %errorlevel%
+echo java -Xmx500M -jar ^%%CONDA_PREFIX^%%\Library\lib\plantuml.jar "$@" %%*> %LIBRARY_BIN%\plantuml.bat
