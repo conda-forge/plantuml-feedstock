890685269880322857a5509a3871ec39ecfbfe04
AjayP <Ajay.Madhavan@in.bosch.com>
2023-02-09 20:38:00 +0530

    Restore error checking during thre build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,17 @@
 CALL gradlew.bat
 CALL gradlew clean build pdfJar -x test
 
-CALL echo F | xcopy .\build\libs\plantuml-pdf-*.jar %LIBRARY_LIB%\plantuml.jar
+CALL if not exist %LIBRARY_LIB% mkdir %LIBRARY_LIB%
 
-echo java -Xmx500M -jar ^%%CONDA_PREFIX^%%\Library\lib\plantuml.jar "$@" %%*> %LIBRARY_BIN%\plantuml.bat
+CALL echo F | xcopy .\build\libs\plantuml-pdf-*.jar %LIBRARY_LIB%\plantuml.jar || goto :error
+
+if not exist %LIBRARY_BIN% mkdir %LIBRARY_BIN%
+
+echo java -Xmx500M -jar %LIBRARY_LIB%\plantuml.jar "$@" %%*> %LIBRARY_BIN%\plantuml.bat
+echo IF %%ERRORLEVEL%% NEQ 0 EXIT /B %%ERRORLEVEL%% >> %LIBRARY_BIN%\plantuml.bat
+
+type %LIBRARY_BIN%\plantuml.bat
+
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
