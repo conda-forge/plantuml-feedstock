11c141b2dca0bc291babc5b24aad7eb92aaa1295
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-02-16 10:33:41 -0600

    fix sed delimiters

--- recipe/build-plantuml.sh
+++ recipe/build-plantuml.sh
@@ -9,9 +9,13 @@ mkdir -p "${PREFIX}/lib" "${PREFIX}/bin"
 
 cp build/libs/plantuml-pdf-*.jar "${PREFIX}/lib/plantuml.jar"
 
-cat << 'EOF' | sed "s/__PREFIX__/${PREFIX}/" > "${PREFIX}/bin/plantuml"
+cat << 'EOF' | sed "s#__PREFIX__#${PREFIX}#" > "${PREFIX}/bin/plantuml"
 #!/usr/bin/env bash
 java -Xmx500M -jar "__PREFIX__/lib/plantuml.jar" "$@"
 EOF
 
 chmod +x "${PREFIX}/bin/plantuml"
+
+echo "generated wrapper script:"
+
+cat "${PREFIX}/bin/plantuml"
